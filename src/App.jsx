d8f20b9156f795aca2683d13c36513903b4790c1
romannikiforov <escaro@ukr.net>
2020-02-19 11:52:19 +0200

    start Project_1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Mini project 1</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export default  [
+    {id: 1, name: 'Dan Abramov', login: 'gaearon',  avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4'},
+    {id: 2, name: 'Sophie Alpert', login: 'sophiebits', avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4'},
+    {id: 3, name: 'Brian Vaughn', login: 'bvaughn', avatar_url: 'https://avatars2.githubusercontent.com/u/199471?v=4'}
+];
